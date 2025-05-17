b67b117cf07bbf248f5f10a3c24b10a4d211677d
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-05-17 14:12:02 +0600

    mongoose db setup

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
-import express from 'express';
-// import cors from 'cors';
-
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 // app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () => {
+  // Connect to MongoDB
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
 });

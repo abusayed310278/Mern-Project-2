4a0d062d6127d4e81af15f13be14c87833622c75
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-05-17 15:16:04 +0600

    prodcut create,delete,get,update

---
+++ backend/routes/product.route.js
@@ -0,0 +1,77 @@
+import express from 'express';
+
+const router = express.Router();
+
+app.post("/api/products", async(req, res) => {
+
+    const product = req.body;
+    // Validate the product data
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({success:false, message: "Please provide all fields." });
+    }
+
+    const newProduct= new Product(product)
+    
+    try {
+        await newProduct.save();
+        res.status(201).json({success:true, message: "Product created successfully", data: newProduct });
+    } catch (error) {
+        console.error("Error creating product:", error.message);
+        res.status(500).json({success:false, message: "Server error" });
+    }
+    
+
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log("Deleting product with ID:", id);
+
+    try {
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({success:false, message: "Product not found" });
+        }
+        res.status(200).json({success:true, message: "Product deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting product:", error.message);
+        res.status(500).json({success:false, message: "Server error" });
+    }
+});
+
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json({success:true, data: products });
+    } catch (error) {
+        console.error("Error fetching products:", error.message);
+        res.status(500).json({success:false, message: "Server error" });
+    }
+});
+
+app.patch("/api/products/:id", async (req, res) => { 
+    const { id } = req.params;
+    const updatedData = req.body;
+
+    try {
+        const product = await Product.findByIdAndUpdate(id, updatedData, { new: true });
+        if (!product) {
+            return res.status(404).json({success:false, message: "Product not found" });
+        }
+        res.status(200).json({success:true, message: "Product updated successfully", data: product });
+    } catch (error) {
+        console.error("Error updating product:", error.message);
+        res.status(500).json({success:false, message: "Server error" });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-// app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
 
 console.log(process.env.MONGO_URI);
 
